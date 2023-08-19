507ba1f88461b1c60e0a40966321fb8b3bdb49d1
samuel <samuel.edmund.morgan@gmail.com>
2023-08-19 16:09:53 +0300

    Modified RV adapter so it can use several types of view holders, learnt about viewType in RV and about RV Pool

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -2,24 +2,32 @@ package com.example.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglist.R
-import com.example.shoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-
+    private lateinit var shopListAdapter: ShopListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupRecyclerView()
 
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-
         viewModel.shopList.observe(this){
-
+            shopListAdapter.shopList = it
         }
+    }
+
+    private fun setupRecyclerView(){
+        shopListAdapter = ShopListAdapter()
+        val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
+        rvShopList.adapter = shopListAdapter
+        rvShopList.recycledViewPool.setMaxRecycledViews(R.layout.item_shop_enabled,ShopListAdapter.MAX_POOL_CAPACITY)
+        rvShopList.recycledViewPool.setMaxRecycledViews(R.layout.item_shop_disabled,ShopListAdapter.MAX_POOL_CAPACITY)
+
     }
 
 
